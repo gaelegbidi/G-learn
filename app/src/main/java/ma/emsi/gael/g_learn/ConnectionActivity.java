b0e6b7b64529ef5b4ac68019cf0351b036445989
gaelegbidi <gaelegbidi@gmail.com>
2017-06-11 18:28:46 +0200

    commit3

--- app/src/main/java/ma/emsi/gael/g_learn/ConnectionActivity.java
+++ app/src/main/java/ma/emsi/gael/g_learn/ConnectionActivity.java
@@ -1,43 +1,41 @@
 package ma.emsi.gael.g_learn;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class ConnectionActivity extends AppCompatActivity {
 
     private LoginButton fbLginButton;
+    private AccessTokenTracker accessTokenTracker ;
+    private CallbackManager callbackManager = CallbackManager.Factory.create();
+    private ProfileTracker profileTracker;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_connection);
 
-//        mVisible = true;
-//        mControlsView = findViewById(R.id.fullscreen_content_controls);
-//        mContentView = findViewById(R.id.fullscreen_content);
-
-
-        // Set up the user interaction to manually show or hide the system UI.
-//        mContentView.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                toggle();
-//            }
-
-
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-        // findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
 
         fbLginButton = (LoginButton)findViewById(R.id.login_button);
         fbLginButton.setReadPermissions("email");
@@ -45,8 +43,32 @@ public class ConnectionActivity extends AppCompatActivity {
         // loginButton.setFragment(this);
         // Other app specific specialization
 
-        CallbackManager callbackManager = CallbackManager.Factory.create();
+
         // Callback registration
+
+
+        accessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(
+                    AccessToken oldAccessToken,
+                    AccessToken currentAccessToken) {
+                // Set the access token using
+                // currentAccessToken when it's loaded or set.
+            }
+        };
+
+        profileTracker = new ProfileTracker() {
+            @Override
+            protected void onCurrentProfileChanged(
+                    Profile oldProfile,
+                    Profile currentProfile) {
+                // App code
+            }
+        };
+
+
+
+        callbackManager =  CallbackManager.Factory.create();
         fbLginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
@@ -57,6 +79,30 @@ public class ConnectionActivity extends AppCompatActivity {
 
                 Toast toast = Toast.makeText(context, text, duration);
                 toast.show();
+                GraphRequest request = GraphRequest.newMeRequest(
+                        loginResult.getAccessToken(),
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(JSONObject object, GraphResponse response) {
+                                String email ="";
+                                try {
+                                    email= response.getJSONObject().getString("email");
+                                } catch (JSONException jse){
+                                    Log.e("ConnectionActivity","jse: "+jse);
+                                }
+                                Log.v("ConnectionActivity", "response.toString: "+response.toString());
+                                Log.v("ConnectionActivity", "object.toString: "+object.toString());
+                                Log.v("ConnectionActivity", "email: "+email);
+
+                                // Application code
+//                                String birthday = object.getString("birthday"); // 01/31/1980 format
+                            }
+                        });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,email,gender,birthday");
+                request.setParameters(parameters);
+                request.executeAsync();
+
             }
 
             @Override
@@ -70,17 +116,44 @@ public class ConnectionActivity extends AppCompatActivity {
 
             }
 
+
+
+
+
+
             @Override
             public void onError(FacebookException exception) {
                 // App code
+                Log.e("ConnectionActivity",exception.toString());
                 Context context = getApplicationContext();
                 CharSequence text = "Cnx error!!";
                 int duration = Toast.LENGTH_SHORT;
-
                 Toast toast = Toast.makeText(context, text, duration);
                 toast.show();
             }
+
+
         });
 
     }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+//        Log.d("ConnectionActivity", data.getData().);
+        Log.d("ConnectionActivity", requestCode+"requestCode");
+        Log.d("ConnectionActivity", resultCode + "resultCode");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        accessTokenTracker.stopTracking();
+        profileTracker.stopTracking();
+    }
+
+
+
 }
